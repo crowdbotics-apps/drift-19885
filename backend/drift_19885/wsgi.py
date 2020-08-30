833a32ef0e90cc94027c0247857534e2bba683b3
Crowdbotics <team@crowdbotics.com>
2020-08-30 09:22:37 +0000

    Update models

--- backend/drift_19885/wsgi.py
+++ backend/drift_19885/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drift_19885.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drift_19885.settings")
 
 application = get_wsgi_application()
